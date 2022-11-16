f5c14e5d2db2db2ece0a835925048fc6637775f2
nagaotani <nagayasu.otani@gmail.com>
2022-11-16 22:41:23 +0900

    delete error message

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import warnings
+
+warnings.simplefilter('ignore')
 
 st.title('株価チャート')
 
-code = '9984.T'
+#st.sidebar.header('銘柄コード入力')
+#code = st.sidebar.text_input('銘柄コード（4桁、半角）') + '.T'
 
-code = st.text_input('銘柄コードを入力してください')
+
+code = st.text_input('銘柄コードを入力')
 code = code + '.T'
-st.write(code)
+
+if len(code) < 6:
+    st.warning('入力お願いします')
+    # 条件を満たないときは処理を停止する
+    st.stop()
+
 
 import pandas_datareader.data as web
 import datetime
@@ -34,4 +44,5 @@ ax1 = fig.add_subplot(1,1,1)
 mpf.plot(df, ax=ax1, style='yahoo', type='candle', xrotation=30)
 
 st.pyplot(fig)
+
 
